995840f6b1bbcf095bb2dc330f54c418db9956f7
ATakaSKY <thakurakash321@gmail.com>
2017-01-02 23:27:54 +0530

    Added scrollspy

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-$('.carousel').carousel({
-  interval: false
+$(function(){
+
+	"use strict";
+
+	var topOffset = 50;
+
+	$('.carousel').carousel({
+  	interval: false
+	});
+
+	// Activate scrollspy
+	$('body').scrollspy({
+	    target: 'header .navbar',
+	    offset: topOffset
+	  });
+
+	 var hash = $(this).find('li.active a').attr('href');
+	  if(hash !== '#featured') {
+	    $('header nav').addClass('inbody');
+	  } else {
+	    $('header nav').removeClass('inbody');
+	  }
+
+
+	  // Add an inbody class to nav when scrollspy event fires
+	  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+	    var hash = $(this).find('li.active a').attr('href');
+	    if(hash !== '#featured') {
+	      $('header nav').addClass('inbody');
+	    } else {
+	      $('header nav').removeClass('inbody');
+	    }
+	  });
+
+
 });
