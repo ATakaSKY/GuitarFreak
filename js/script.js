e13bf4963e85da8b2028157b5dec9536086e1f6a
ATakaSKY <thakurakash321@gmail.com>
2017-01-02 23:36:02 +0530

    added smooth scroll

--- js/script.js
+++ js/script.js
@@ -32,5 +32,19 @@ $(function(){
 	    }
 	  });
 
+	  //Added smooth scroll
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top-topOffset+2
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+
 
 });
