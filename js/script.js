2c81da50df58404c92301b9f4085efa5a0ed70b6
ATakaSKY <thakurakash321@gmail.com>
2017-01-03 00:49:41 +0530

    Fade carousel and display it in randomized fashion

--- js/script.js
+++ js/script.js
@@ -4,10 +4,25 @@ $(function(){
 
 	var topOffset = 50;
 	var slideqty = $('#featured .item').length;
+	var wheight = $(window).height(); //get the height of the window
+	var randSlide = Math.floor(Math.random()*slideqty);
 
-	$('.carousel').carousel({
-  	interval: false
-	});
+  	$('#featured .item').eq(randSlide).addClass('active');
+  
+  	$('.fullheight').css('height', wheight); //set to window tallness  
+
+  	//replace IMG inside carousels with a background image
+	  $('#featured .item img').each(function() {
+	    var imgSrc = $(this).attr('src');
+	    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+	    $(this).remove();
+	  });
+
+	  //adjust height of .fullheight elements on window resize
+	  $(window).resize(function() {
+	    wheight = $(window).height(); //get the height of the window
+	    $('.fullheight').css('height', wheight); //set to window tallness  
+	  });
 
 	// Activate scrollspy
 	$('body').scrollspy({
@@ -34,24 +49,35 @@ $(function(){
 	  });
 
 	  //Added smooth scroll
-	  $('a[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	  $('.navbar a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') === 
+	      this.pathname.replace(/^\//,'') && 
+	      location.hostname === this.hostname) {
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 	      if (target.length) {
-	        $('html, body').animate({
+	        $('html,body').animate({
 	          scrollTop: target.offset().top-topOffset+2
-	        }, 1000);
+	        }, 500);
 	        return false;
-	      }
-	    }
-	  });
+	      } //target.length
+	    } //click function
+	  }); //smooth scrolling
 
 
 	  //Automatically generate carousel indicators
-	  for (var i=0; i < slideqty; i++) {
-	    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+	    for (var i=0; i < slideqty; i++) {
+	    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+	    if (i === randSlide) {
+	      insertText += ' class="active" ';
+	    }
+	    insertText += '></li>';
 	    $('#featured ol').append(insertText);
 	  }
+
+
+	  $('.carousel').carousel({
+  		pause: false
+	});
 
 });
